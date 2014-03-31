53e12fe530474431d3488b2f634977730e555b12
Matt Webster <mattywebster@gmail.com>
2014-03-31 13:08:31 -0700

    added clients for each OS

---
+++ App/iOS/Deploy/AOTBuildOutput7342543535703285767.tmp/extensionglue.asm
@@ -0,0 +1,14 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.section	__TEXT,__textcoal_nt,coalesced,pure_instructions
+	.section	__TEXT,__const_coal,coalesced
+	.section	__TEXT,__picsymbolstub4,symbol_stubs,none,16
+	.syntax unified
+	.section	__TEXT,__literal8,8byte_literals
+	.globl	_g_com_adobe_air_fre_fmap
+	.align	3
+_g_com_adobe_air_fre_fmap:
+	.space	8
+
+
+
+.subsections_via_symbols
